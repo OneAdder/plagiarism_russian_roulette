8d894aec4944356ba86b51102debff1ffef97a13
OneAdder <mikivo@list.ru>
2019-05-28 17:45:24 +0300

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# Russian Roulette
+This Python tool reads the text of your thesis in odf format and adds plagiarism with 1/6 probability.
+
+The inserted text is taken from the Introduction of the [WALS language database](https://wals.info/chapter/s1).
+
+To work with this tool you just need to put the file of your thesis in ODT format (name should also end with '.odt') into the module directory.
+
+
---
+++ russian_roulette.py
@@ -0,0 +1,21 @@
+import random
+import os
+from odf.opendocument import load
+from odf.text import *
+
+with open('wals_intro', 'r', encoding='utf-8') as f:
+    pl = f.read()
+    
+filename = [f for f in os.listdir() if f.endswith('.odt')][0]
+
+doc = load(filename)
+
+h = H(outlinelevel=1, text="Plagiarism")
+doc.text.addElement(h)
+
+p = P(text=pl)
+doc.text.addElement(p)
+
+pool = range(6)
+if random.choice(pool) == 4:
+    doc.save('test')
